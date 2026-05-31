69577fafea5b9c212d85ce67fd3f409742b8a0b7
jgabrielcybr <hollowfx35@gmail.com>
2026-05-30 21:42:36 -0700

    Refactor main and countPerfect functions
    Refactor main function and countPerfect function for clarity.

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -5,17 +5,32 @@ using namespace std;
 // Function prototype
 int countPerfect(const int scores[], int size);
 
-int main() {
+int main()
+{
     int scores[20];
     int count = 0;
     int input;
 
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
 
+    for(int i = 0; i <= 19; i++)
+    {
+        cin >> input;
+        if (input == -1)
+        {
+            break;
+        }
+        else
+        {
+            scores[i] = input;
+            count++;
+        }
+    }
+
     // Store scores and keep track of how many are entered
     // Report perfect scores using the helper function
   // COMPLETE THE FUNCTION BELOW
-  int perfectOnes = countPerfect(scores, count);
+    int perfectOnes = countPerfect(scores, count);
     
     cout << "\nYou entered " << count << " scores." << endl;
     cout << "Perfect scores (100): " << perfectOnes << endl;
@@ -26,6 +41,15 @@ int main() {
 // Value-returning function to count scores of 100
 int countPerfect(const int scores[], int size) {
     int totalPerfect = 0;
-    //COMPLETE THE CODE 
+    //COMPLETE THE CODE
+    
+    for(int i = 0; i <= size - 1; i++)
+    {
+        if(scores[i] == 100)
+        {
+            totalPerfect++;
+        }
+    }
+
     return totalPerfect;
 }
