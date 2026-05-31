843b5b31168a4759f38ce8e76109dbc0a8a9400b
jgabrielcybr <hollowfx35@gmail.com>
2026-05-30 20:57:59 -0700

    Phone Book Assignment
    Refactor main function for clarity and structure.

--- ArraysAssignment1.cpp
+++ ArraysAssignment1.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
 //add includes
 //add namespace declarations
 string lookupName(string targetName, string names[], string phoneNumbers[], int size);
@@ -5,34 +8,43 @@ string lookupName(string targetName, string names[], string phoneNumbers[], int 
 int main()
 {
     using namespace std;
-    string names[] = {"Michael Myers",
-                      "Ash Williams",
-                      "Jack Torrance",
-                      "Freddy Krueger"};
-    string phoneNumbers[] = {"333-8000","333-2323",
-                             "333-6150","339-7970"};
+    string names[] = {"Michael Myers", "Ash Williams", "Jack Torrance", "Freddy Krueger"};
+    string phoneNumbers[] = {"333-8000","333-2323", "333-6150","339-7970"};
     string targetName, targetPhone;
     char c;
     do
     {
-         cout << "Enter a name to find the "
-              << "corresponding phone number."
-              << endl;
-         getline(cin, targetName);
-         targetPhone = lookupName(targetName,
-                           names, phoneNumbers,4);
-         if (targetPhone.length() > 0)
-                 cout << "The number is: "
-                      << targetPhone << endl;
-         else
-                 cout << "Name not found. "
-                      << endl;
-         cout << "Look up another name? (y/n)"
-              << endl;
-      cin > c;
-      cin.ignore();
- } while (c == 'y');
- return 0;
+    cout << "Enter a name to find the corresponding phone number." << endl;
+    getline(cin, targetName);
+    targetPhone = lookupName(targetName, names, phoneNumbers, 4);
+    if (targetPhone.length() > 0)
+    {
+        cout << "The number is: " << targetPhone << endl;
+    }
+    else
+    {
+        cout << "Name not found. " << endl;
+        cout << "Look up another name? (y/n)" << endl;
+        cin >> c;
+        cin.ignore();
+    }
+    }while (c == 'y');
+    return 0;
 }
 
 //complete lookupName unction
+
+string lookupName(string targetName, string names[], string phoneNumbers[], int size)
+{
+    string targetPhoneNumber;
+
+    for(int i = 0; i <= 3; i++)
+    {
+        if(names[i] == targetName)
+        {
+            targetPhoneNumber = phoneNumbers[i];
+        }
+    }
+
+    return targetPhoneNumber;
+}
